5df9d3e16f54a609c6f078ad83ffdd609ee94d07
marcoz34 <marcoz34@correo.es>
2022-11-10 19:18:57 +0100

    Muestro2

--- PrimeraAplicacion/src/primeraaplicacion/SegundaClase.java
+++ PrimeraAplicacion/src/primeraaplicacion/SegundaClase.java
@@ -3,6 +3,7 @@ public class SegundaClase {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
+        System.out.println("c");
     }
  
 }
