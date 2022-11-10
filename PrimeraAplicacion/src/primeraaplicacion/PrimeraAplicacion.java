aa53aa82f0d3b847bc7b0ede4f94a17f478709c8
marcos <marcos@10.0.2.15>
2022-11-10 19:01:37 +0100

    Hola mundo

--- PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
+++ PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
@@ -1,7 +1,7 @@
 package primeraaplicacion;
 public class PrimeraAplicacion {
     public static void main(String[] args) {
-        
+        System.out.println("Hola mundo");
             }
     
 }
